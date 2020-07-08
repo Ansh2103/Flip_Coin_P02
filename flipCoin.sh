28c6d12961c5a4cdd2b3b569872a7675db0d222d
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-07-08 05:32:50 +0530

    Added : Flipping of a coin multiple times

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,17 +1,35 @@
 #!/bin/bash -x
-#Declaring Variable
-count=0
+echo "Welcome To Flip Coin Simulation "
 
-#Taking Input From User
-read -p "Enter no. of times want to repeat the loop" number
-while [[ $count -ne $number ]]
+#UseCase1- As a Simulator start with Flipping a Coin to Display Heads or Tails as winner.
+isHead=0;
+randomCheck=$(( RANDOM%2 ))
+
+if [[ isHead -eq randomCheck ]]
+then
+	echo "Head is winner"
+else
+	echo "Tail is winner"
+fi
+
+#UseCase2- As a simulator, loop through Multiple times of flipping a coin and show number of times head and tail has won
+isHead=0;
+isHeadWon=0;
+isTailWon=0;
+
+for (( count=0; count<=10; count++ ))
 do
-        random=$((RANDOM%2))
-        if [[ $random -eq 1 ]]
-        then
-                echo "Head"
-        else
-                echo "Tail"
-        fi
-        ((count++))
+	randomCheck1=$(( RANDOM%2 ))
+
+if [[ isHead -eq randomCheck1 ]]
+then
+	echo "Head"
+	isHeadWon=$(( $isHeadWon + 1 ))
+else
+	echo "Tail"
+	isTailWon=$(( $isTailWon + 1 ))
+fi
 done
+
+echo "Total Number Of Head Win is:  $isHeadWon"
+echo "Total Number Of Tail Win is: $isTailWon"
